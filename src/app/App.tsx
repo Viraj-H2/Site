c0bebb8b6d2b9c49ea6f7c2e8b699c19d066c264
Adrien Soursou <adrien.soursou@viraj-h2.com>
2024-09-03 19:46:25 +0200

    Added Bootstrap 5

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,16 @@
 import { useState } from 'react'
-import virajH2Logo from '/favicon.svg'
-import './App.css'
+import logo from '/logo.svg'
+import { Button } from 'react-bootstrap'
 
 export default function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <img src={virajH2Logo} className="logo" alt="Viraj H2 logo" />
-      <h1>Viraj H2</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          {count === 0 ? 'Send love' : `${count} ❤️`}
-        </button>
-      </div>
+      <img src={logo} />
+      <Button variant="primary" onClick={() => setCount((count) => count + 1)}>
+        {count === 0 ? 'Send love' : `${count} ❤️`}
+      </Button>
     </>
   )
 }
