ef2c61f7da4a9f2df26ad8784b78be492d9541c4
Adrien Soursou <adrien.soursou@viraj-h2.com>
2024-09-23 17:28:06 +0200

    Added sponsors section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -14,6 +14,13 @@ interface TeamSection {
   members: TeamMemberData[];
 }
 
+const sponsors = [
+  "fondation_jjll.png",
+  "isae_supaero.png",
+  "we_spark_the_future.jpg",
+  "fondation_isae_supaero.svg"
+];
+
 export default function About() {
   const [teamSections, setTeamSections] = useState<TeamSection[]>([]);
 
@@ -27,18 +34,30 @@ export default function About() {
     <Container fluid>
       <Row className=''>
         <Col>
-        {teamSections.map((section, sectionIndex) => (
-          <Row key={sectionIndex} className='m-5'>
-            <Col className='border-top'>
-              <h2 className='text-center justify-content-center m-5'>{section.sectionTitle}</h2>
+          {teamSections.map((section, sectionIndex) => (
+            <Row key={sectionIndex} className='m-5'>
+              <Col className='border-top'>
+                <h2 className='text-center justify-content-center m-5'>{section.sectionTitle}</h2>
+                <Row className='justify-content-evenly'>
+                  {section.members.map((member, memberIndex) => (
+                    <TeamMember key={memberIndex} {...member} />
+                  ))}
+                </Row>
+              </Col>
+            </Row>
+          ))}
+          <Row key='#sponsors' className='m-5'>
+              <Col className='border-top'>
+              <h2 className='text-center justify-content-center m-5'>Ils nous soutiennent</h2>
               <Row className='justify-content-evenly'>
-                {section.members.map((member, memberIndex) => (
-                  <TeamMember key={memberIndex} {...member} />
+                {sponsors.map((sponsor, index) => (
+                  <Col key={index} xs={4} md={2} className='text-center'>
+                    <img src={`/assets/images/sponsors/${sponsor}`} alt={sponsor} className='img-fluid' />
+                  </Col>
                 ))}
-              </Row>
-            </Col>
-          </Row>
-        ))}
+                </Row>
+              </Col>
+            </Row>
         </Col>
       </Row>
     </Container>
