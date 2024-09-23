c88be9d94377eec5abd1da71d20f468df93713eb
Adrien Soursou <adrien.soursou@viraj-h2.com>
2024-09-23 23:35:08 +0200

    Changed "Newsletter" section to "Press"

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ export default function Navbar() {
             <NavDropdown title='À propos'>
               <NavDropdown.Item as={Link} to='/about#team'>L'équipe</NavDropdown.Item>
               <NavDropdown.Item as={Link} to='/about#sponsors'>Soutiens</NavDropdown.Item>
-              <NavDropdown.Item as={Link} to='/about#newsletter'>Newsletter</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to='/about#press'>Presse</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link as={Link} to='/concept'>Notre concept</Nav.Link>
             <Nav.Link as={Link} to='/careers'>Carrières</Nav.Link>
