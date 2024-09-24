3729f032c1b5772dce314c5b9310c8cdeb3642b9
Adrien Soursou <adrien.soursou@viraj-h2.com>
2024-09-24 08:38:17 +0200

    Added more information about Careers page

--- src/pages/Careers.tsx
+++ src/pages/Careers.tsx
@@ -1,8 +1,14 @@
-import { Container } from 'react-bootstrap';
+import { Col, Container } from 'react-bootstrap';
 
 export default function Careers() {
   return (
     <Container fluid>
+      <Col className='text-center justify-content-center'>
+        <h1>Offres</h1>
+        <p className='info'>
+          Cette section liste les offres d'emploi actuellement disponibles chez Viraj H2. L'utilisateur a la possibilité de filtrer et de trier les offres en fonction de différents critères.
+        </p>
+      </Col>
     </Container>
   );
 }
