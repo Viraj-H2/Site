7b657ca0613b22b073989a43b320d1a94f8b9486
Adrien Soursou <adrien.soursou@viraj-h2.com>
2024-09-24 08:39:47 +0200

    Removed concept image which is already on homepage

--- src/pages/Concept.tsx
+++ src/pages/Concept.tsx
@@ -5,13 +5,6 @@ export default function Concept() {
     <Container fluid>
       <Col className='text-center justify-content-center'>
         <h1>Notre concept</h1>
-        <Image src='/assets/images/concept.png' className='img-fluid w-75' alt='Concept' />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat semper velit, et cursus dolor luctus vitae. Donec mollis quam non neque aliquam interdum. Nullam et iaculis est, vitae pellentesque purus. Proin mattis tellus ullamcorper, cursus diam quis, rhoncus nulla. Quisque molestie nunc a diam vulputate hendrerit. Fusce eget maximus risus. In aliquam, nulla non interdum egestas, lectus ex congue magna, et cursus nulla purus id felis. Sed mi eros, sollicitudin ut finibus vestibulum, maximus ut risus. Sed ut mi quis dui rutrum aliquet ac id justo. Ut nec sapien sed sem tincidunt vestibulum. Suspendisse molestie, nisi in fringilla interdum, mi neque ornare sem, nec lacinia arcu lacus ut lacus. Duis et iaculis purus.
-        </p>
-        <p>
-          Vestibulum nulla mi, tempor a nunc at, porta ullamcorper mauris. Duis a gravida mi, quis placerat massa. Etiam id nisl id sapien imperdiet vehicula eget eu lectus. Curabitur in vulputate enim. Pellentesque elementum pellentesque elit at molestie. Curabitur feugiat ante elit, vitae viverra libero fringilla eget. Sed a orci at magna cursus consequat facilisis vitae quam. Pellentesque magna leo, sollicitudin in suscipit et, maximus nec tortor. Sed lorem ex, feugiat et nibh non, gravida finibus ante.
-        </p>
         <Image src='/assets/images/flow_diagram.png' className='img-fluid w-75' alt='Flow diagram' />
         <p>
           hasellus quis porttitor enim, at ullamcorper dui. Fusce id quam arcu. Suspendisse ultrices maximus rutrum. Sed elementum dui mi. Suspendisse purus leo, dapibus in nibh sed, pulvinar cursus sem. Donec aliquam felis sed turpis aliquet, sit amet pulvinar velit molestie. Donec convallis, ligula sed consectetur ultricies, massa enim interdum metus, vitae convallis erat velit quis massa. Donec et sem a elit tincidunt vehicula. Maecenas finibus sem nec sagittis imperdiet. Ut aliquam sit amet ipsum non lobortis. In nisl ipsum, efficitur non facilisis nec, sollicitudin id lacus. Pellentesque porttitor at elit at bibendum. Duis sit amet tellus sapien. Nulla ante eros, pharetra nec arcu eu, molestie semper ligula. Cras ut velit eu ante interdum pellentesque in vitae tellus.
