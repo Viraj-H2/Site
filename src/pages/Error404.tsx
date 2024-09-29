0702854e4f24da55fd75ed8260ea0beee773df9c
Adrien Soursou <adrien.soursou@viraj-h2.com>
2024-09-29 23:32:06 +0200

    Added cool design for 404 page

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,5 +1,9 @@
+import { Container, Image } from 'react-bootstrap';
+
 export default function Error404() {
   return (
-    <p>404 Not Found</p>
+    <Container fluid className='d-flex justify-content-center align-items-center' style={{ height: '100vh' }}>
+      <Image src='/assets/images/404.png' className='img-fluid' style={{ maxWidth: '100%', maxHeight: '100%' }} />
+    </Container>
   );
 }
