1de514ac76e2d2fa16331beffa14956182375773
Adrien Soursou <adrien.soursou@viraj-h2.com>
2024-09-30 10:40:32 +0200

    Added newsletter modal

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { Container, Row, Col, Form, Button, Nav } from 'react-bootstrap';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
@@ -7,9 +6,6 @@ import { Link } from 'react-router-dom';
 import ContactEmail from '../../common/components/ContactEmail';
 
 export default function Footer() {
-  const [email, setEmail] = useState('');
-  const [validated, setValidated] = useState(false);
-
   const links = [
     { to: '/', text: 'Accueil' },
     { to: '/concept', text: 'Notre concept' },
@@ -18,27 +14,17 @@ export default function Footer() {
     { to: '/blog', text: 'Blog' }
   ];
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
-    setValidated(true);
-  };
-
   return (
     <footer className='py-4 border-top'>
       <Container>
-        <Row className='m-0 justify-content-evenly'>
+        <Row className='m-0 justify-content-between'>
           <Col className='mb-3'>
             <h5 className='text-uppercase mb-3'>Coordonnées</h5>
             <Row>
               <Col xs={1} className='d-flex align-items-center'>
-                <FontAwesomeIcon icon={faMapMarkerAlt} className='me-2' />
+                <FontAwesomeIcon icon={faMapMarkerAlt} className='ms-3' />
               </Col>
-              <Col className='d-flex align-items-center'>
+              <Col className='d-flex align-items-left'>
                 <address>
                   <b>Viraj H2</b><br />
                   10 Avenue Edouard Belin<br />
@@ -49,7 +35,7 @@ export default function Footer() {
             </Row>
             <Row>
               <Col xs={1} className='d-flex align-items-center'>
-                <FontAwesomeIcon icon={faEnvelope} className='me-2' />
+                <FontAwesomeIcon icon={faEnvelope} className='ms-3' />
               </Col>
               <Col className='d-flex align-items-center'>
                 <ContactEmail />
@@ -68,26 +54,6 @@ export default function Footer() {
                 </Nav.Item>
               ))}
             </Nav>
-          </Col>
-
-          <Col sm={4} className='min-width-col'>
-            <h5 className='text-uppercase mb-3'>Newsletter</h5>
-            <p>Restez informé de nos dernières actualités</p>
-            <Form noValidate validated={validated} onSubmit={handleSubmit}>
-              <Row className='d-flex flex-wrap align-items-start m-0'>
-                <Col xs={12} md='auto' className='mt-1 flex-grow-1 p-1'>
-                  <Form.Group controlId='newsletter-email'>
-                    <Form.Control type='email' placeholder='Adresse email' aria-label='Email address' autoComplete='email' value={email} onChange={(e) => setEmail(e.target.value)} required />
-                      <Form.Control.Feedback type='invalid'>
-                        Veuillez entrer une adresse email valide.
-                      </Form.Control.Feedback>
-                    </Form.Group>
-                </Col>
-                <Col md='auto' className='mt-1 d-flex justify-content-end p-1'>
-                  <Button variant='primary' type='submit' className='w-md-auto w-100'>S'inscrire</Button>
-                </Col>
-              </Row>
-            </Form>
           </Col>
         </Row>
 
