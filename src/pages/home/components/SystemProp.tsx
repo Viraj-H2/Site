f50d12f743e763baf50548bb793621e4d98efaf9
Adrien Soursou <adrien.soursou@viraj-h2.com>
2024-09-30 09:46:56 +0200

    Fix cards height

--- src/pages/home/components/SystemProp.tsx
+++ src/pages/home/components/SystemProp.tsx
@@ -10,15 +10,14 @@ interface SystemPropsProps {
 
 export default function SystemProps({ icon, title, description }: SystemPropsProps) {
   return (
-    <Col className='align-items-center text-center'>
-      <Card border='primary' className='m-2'>
-      <FontAwesomeIcon icon={icon} className='mt-4 display-4 text-primary' />
-      <Card.Body>
-        <Card.Title className='text-primary'>{title}</Card.Title>
-        <Card.Text>{description}</Card.Text>
-      </Card.Body>
-    </Card>
-    
-  </Col>
+    <Col xs={12} sm={6} md={4} lg={3} className='d-flex align-items-stretch' style={{ minWidth: '250px' }}>
+      <Card border='primary' className='m-2 w-100 text-center'>
+        <Card.Body className='d-flex flex-column'>
+          <FontAwesomeIcon icon={icon} className='mt-2 mb-3 display-4 text-primary' />
+          <Card.Title className='text-primary'>{title}</Card.Title>
+          <Card.Text className='flex-grow-1'>{description}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
   );
-}
+};
