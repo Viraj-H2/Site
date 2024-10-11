cc073bceb752d08720c9972dfed59e99a30d804a
Adrien Soursou <adrien.soursou@viraj-h2.com>
2024-10-11 09:55:37 +0200

    Update ContactEmail component to include subject in mailto link

--- src/common/components/ContactEmail.tsx
+++ src/common/components/ContactEmail.tsx
@@ -1,6 +1,6 @@
 export default function ContactEmail() {
   return (
-    <a href='mailto:contact@viraj-h2.com' className='text-decoration-none'>
+    <a href='mailto:contact@viraj-h2.com&subject=Demande%20de%20contact' className='text-decoration-none'>
       <b>contact@viraj-h2.com</b>
     </a>
   );
