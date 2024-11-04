0d6a3440983a5bc23f48dd9bbb209ba328e61a0b
Risson <aime.risson.1@gmail.com>
2024-11-04 15:06:48 +0100

    hash router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Footer from './components/Footer';
 import Navbar from './components/Navbar'
 import Blog from '../pages/blog/Blog';
@@ -11,19 +11,19 @@ import BlogPost from '../pages/blog/components/BlogPost';
 import { PostsProvider } from '../pages/blog/providers/PostsProvider';
 
 export default function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/concept' element={<Concept />} />
-        <Route path='/team' element={<Team />} />
-        <Route path='/careers' element={<Careers />} />
-        <Route path="/blog" element={<PostsProvider><Blog /></PostsProvider>} />
-        <Route path="/blog/:title" element={<PostsProvider><BlogPost /></PostsProvider>} />
-        <Route path='*' element={<Error404 />} />
-      </Routes>
-      <Footer />
-    </Router>
-  )
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/concept' element={<Concept />} />
+                <Route path='/team' element={<Team />} />
+                <Route path='/careers' element={<Careers />} />
+                <Route path="/blog" element={<PostsProvider><Blog /></PostsProvider>} />
+                <Route path="/blog/:title" element={<PostsProvider><BlogPost /></PostsProvider>} />
+                <Route path='*' element={<Error404 />} />
+            </Routes>
+            <Footer />
+        </Router>
+    )
 }
